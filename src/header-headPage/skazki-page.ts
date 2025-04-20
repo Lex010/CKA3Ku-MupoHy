6065f1544d1d1d3e508e31bafa0ece07467e1af7
Lex Mel <lex07672@gmail.com>
2025-04-20 14:26:30 +0300

    feat: skazki page h1 and styles for it

--- src/header-headPage/skazki-page.ts
+++ src/header-headPage/skazki-page.ts
@@ -4,6 +4,7 @@ import allTales from '../data-fairy-tale/all-tales';
 export default function skazkiPage(container: HTMLElement) {
   const main = container;
 
+  createElement('h1', container, { class: 'fairy-title' }, 'Книга рассказов');
   const listWrapper = createElement('div', container, { class: 'fairy-list' });
   const showPushistayaPlaneta = createElement('div', listWrapper, { class: 'ckazki' }, 'Пушистая планета');
   showPushistayaPlaneta.addEventListener('click', () => {
