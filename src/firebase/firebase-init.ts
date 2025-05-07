7ee40426d312a0fab55243a2e1c09a2faa9b015b
Lex Mel <lex07672@gmail.com>
2025-05-07 15:46:15 +0300

    feat: firebase-init.ts and for change tsconfig for correct import from modules

---
+++ src/firebase/firebase-init.ts
@@ -0,0 +1,17 @@
+// Инициализация Firebase
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBLIubyI_VRb6O2zzNEITFS7X-ys-fW6DI',
+  authDomain: 'cka3ku-mupohy.firebaseapp.com',
+  projectId: 'cka3ku-mupohy',
+  appId: '1:556680336729:web:f0e99b9a1ccba9611be848',
+  measurementId: 'G-7GVESXHD4V',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
