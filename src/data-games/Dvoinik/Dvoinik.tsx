0d450ca8d9ef17c09de920cac34c775ce215d9f8
Lex Mel <lex07672@gmail.com>
2025-06-04 18:06:35 +0300

    feat: dvoiniki: set player count

--- src/data-games/Dvoinik/Dvoinik.tsx
+++ src/data-games/Dvoinik/Dvoinik.tsx
@@ -11,11 +11,13 @@ const idDvoinikGame = {
 // Допустимые количества уникальных карточек
 const difficultyLevels = [2, 4, 8];
 const fieldSizes = [16, 24, 32];
+const playersCount = [1, 2, 3];
 
 const Dvoinik: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [uniqueCardCount, setUniqueCardCount] = useState<number>(difficultyLevels[0]);
-  const [fieldSize, setFieldSize] = useState<number>(fieldSizes[0]);
+  const [fieldSiz, setFieldSize] = useState<number>(fieldSizes[0]);
+  const [plaCount, setPlaCount] = useState<number>(playersCount[0]);
 
   const handleStartGame = () => {
     setIsModalOpen(true);
@@ -47,10 +49,28 @@ const Dvoinik: React.FC = () => {
             {fieldSizes.map((size) => (
               <button
                 key={size}
-                className={`difficulty-button-dvoiniki ${fieldSize === size ? 'selected' : ''}`}
+                className={`difficulty-button-dvoiniki ${fieldSiz === size ? 'selected' : ''}`}
                 onClick={() => setFieldSize(size)}
               >
                 {size}
+              </button>
+            ))}
+          </div>
+        </div>
+      </details>
+
+      <details className="dvoiniki-details">
+        <summary className="dvoiniki-summary">👤 Количество игроков</summary>
+        <div className="difficulty-selection-dvoiniki">
+          <p>Игроки:</p>
+          <div className="difficulty-buttons-dvoiniki">
+            {playersCount.map((count) => (
+              <button
+                key={count}
+                className={`difficulty-button-dvoiniki ${plaCount === count ? 'selected' : ''}`}
+                onClick={() => setPlaCount(count)}
+              >
+                {count}
               </button>
             ))}
           </div>
@@ -65,7 +85,7 @@ const Dvoinik: React.FC = () => {
         <GameModal
           onClose={() => setIsModalOpen(false)}
           headerContent={<HeaderDvoinik />}
-          gameFieldContent={<MainFieldDvoiniki uniqueCardCount={uniqueCardCount} fieldSize={fieldSize} />}
+          gameFieldContent={<MainFieldDvoiniki uniqueCardCount={uniqueCardCount} fieldSize={fieldSiz} />}
         />
       )}
     </div>
