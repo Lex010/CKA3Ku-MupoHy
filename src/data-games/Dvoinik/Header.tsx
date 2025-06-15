52dc50776bdfccb1a73f3d0267b1bb5393c1fa1e
Lex Mel <lex07672@gmail.com>
2025-06-15 17:10:17 +0300

    feat: dvoiniki: change txt for symbols in game statistics

--- src/data-games/Dvoinik/Header.tsx
+++ src/data-games/Dvoinik/Header.tsx
@@ -25,8 +25,18 @@ const HeaderDvoinik: React.FC<HeaderDvoinikProps> = ({ playerStats, playerNames 
         <div className="header-stats-popup--dvoinik">
           {playerStats.map((stat, index) => (
             <div key={index} className="header-player-stat--dvoinik">
-              <strong>{playerNames[index] || `Игрок ${index + 1}`}</strong>: <br />
-              Собрано: {stat.matches} | Ошибок: {stat.mistakes} | Побед: {stat.wins}
+              <strong>{playerNames[index] || `Первый`}</strong>: <br />
+              <span className="stat-item--dvoinik">
+                <span className="icon-check--dvoinik" />
+                {stat.matches}
+              </span>
+              {' | '}
+              <span className="stat-item--dvoinik">
+                <span className="icon-cross--dvoinik" />
+                {stat.mistakes}
+              </span>
+              {' | '}
+              Побед: {stat.wins}
             </div>
           ))}
         </div>
