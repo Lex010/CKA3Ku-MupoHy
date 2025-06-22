61259d03cfcb3b4f7e7a2b1bdd77a890bae9fcfb
Lex Mel <lex07672@gmail.com>
2025-06-22 14:44:33 +0300

    feat: delete canvas animation background element

--- src/header-mainPage/MainPage.tsx
+++ src/header-mainPage/MainPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { mainData } from '../site-manager-object/mainData';
 import Tooltip from '../utils/Tooltip';
-// import BackgroundParticles from '../utils/BackgroundAnimation/BackgroundParticles';
 
 const MainPage: React.FC = () => {
   const navigate = useNavigate();
@@ -10,28 +9,26 @@ const MainPage: React.FC = () => {
   const menuItems = Object.values(mainData).filter((item) => item.type === 'menu');
 
   return (
-    // <BackgroundParticles>
-      <div>
-        <h1 className="page-title">
-          Главная - "МИР"{' '}
-          <Tooltip text="МИР — Мирону Интересные Развлечения">
-            <span className="page-tooltipTriger">?</span>
-          </Tooltip>
-        </h1>
-        <div className="page-list">
-          {menuItems.map((item) => (
-            <div
-              key={item.id}
-              className="page-unit"
-              onClick={() => navigate(`/${item.id}`)}
-              style={{ cursor: 'pointer' }}
-            >
-              {item.title}
-            </div>
-          ))}
-        </div>
+    <div>
+      <h1 className="page-title">
+        Главная - "МИР"{' '}
+        <Tooltip text="МИР — Мирону Интересные Развлечения">
+          <span className="page-tooltipTriger">?</span>
+        </Tooltip>
+      </h1>
+      <div className="page-list">
+        {menuItems.map((item) => (
+          <div
+            key={item.id}
+            className="page-unit"
+            onClick={() => navigate(`/${item.id}`)}
+            style={{ cursor: 'pointer' }}
+          >
+            {item.title}
+          </div>
+        ))}
       </div>
-    // </BackgroundParticles>
+    </div>
   );
 };
 
