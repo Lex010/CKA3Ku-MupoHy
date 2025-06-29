9ef57ccb66938bfc82fe53be9ed3667b6c3ff306
Lex Mel <lex07672@gmail.com>
2025-06-29 15:44:15 +0300

    feat: change bg game anim to baloons

--- src/utils/BackgroundAnimation/GamePage/GamePageAnimation.tsx
+++ src/utils/BackgroundAnimation/GamePage/GamePageAnimation.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './GamePageAnimation.css';
 
+const COLORS = ['#c2428a', '#9045b5', '#457bb5', '#c43342', '#714bbd'];
+
 const GamePageAnimation: React.FC = () => {
   return (
-    <div className="game-page-animation">
-      <ul className="game-page-animation__circles">
-        {Array.from({ length: 10 }, (_, i) => (
-          <li key={i}></li>
-        ))}
-      </ul>
+    <div className="game-page-animation__balloons-cont">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div
+          key={i}
+          className={`game-page-animation__balloon`}
+          style={{
+            backgroundColor: COLORS[i % COLORS.length],
+            left: `${Math.random() * 100}%`,
+            animationDuration: `${Math.random() * 20 + 7}s`,
+            animationDelay: `-${Math.random() * 50 + 50}s`,
+          }}
+        >
+          <div
+            className="game-page-animation__balloon-knot"
+            style={{
+              borderBottomColor: COLORS[i % COLORS.length],
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
 };
