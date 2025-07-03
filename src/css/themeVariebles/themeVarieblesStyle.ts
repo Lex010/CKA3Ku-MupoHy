787674772a4fcdf9b6f18b0aaf2e4f3d18ab99ab
Lex Mel <lex07672@gmail.com>
2025-07-03 14:48:13 +0300

    feat: add another day/night theme variable, release update

--- src/css/themeVariebles/themeVarieblesStyle.ts
+++ src/css/themeVariebles/themeVarieblesStyle.ts
@@ -1,9 +1,13 @@
 export const dayTheme = {
+  '--bgHtml': 'linear-gradient(to bottom, #cceaff, #ffffff)',
+  '--headerBackground': '#fff0e5d5',
   '--txtLeftBorder': '#ffb347',
   '--txtBackground': '#fff',
 };
 
 export const nightTheme = {
+  '--bgHtml': 'linear-gradient(to bottom, #364b61, #9ccbfa)',
+  '--headerBackground': '#3269a3cf',
   '--txtLeftBorder': '#c7925b',
-  '--txtBackground': '#abd7a5',
+  '--txtBackground': '#9ccbfa',
 };
