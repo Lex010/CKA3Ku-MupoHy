7e277293760b0bf44e204eca5f3800234c8b986c
Lex Mel <lex07672@gmail.com>
2025-07-03 20:11:01 +0300

    feat: add another day/night theme variable(2), disable hover effects on touch devices(not all), release update

--- src/css/themeVariebles/themeVarieblesStyle.ts
+++ src/css/themeVariebles/themeVarieblesStyle.ts
@@ -1,13 +1,19 @@
 export const dayTheme = {
   '--bgHtml': 'linear-gradient(to bottom, #cceaff, #ffffff)',
   '--headerBackground': '#fff0e5d5',
+  '--h1AndHover': '#ff6f61',
+  '--h1AndHoverOPACITY': '#ff6e6186',
   '--txtLeftBorder': '#ffb347',
+  '--mainBackground': '#fff8f0',
   '--txtBackground': '#fff',
 };
 
 export const nightTheme = {
   '--bgHtml': 'linear-gradient(to bottom, #364b61, #9ccbfa)',
   '--headerBackground': '#3269a3cf',
+  '--h1AndHover': '#b37eff',
+  '--h1AndHoverOPACITY': '#b37eff7a',
   '--txtLeftBorder': '#c7925b',
+  '--mainBackground': '#cedfff',
   '--txtBackground': '#9ccbfa',
 };
