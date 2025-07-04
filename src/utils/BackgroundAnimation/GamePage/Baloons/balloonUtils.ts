cab8b8c4fe9437aedd33becd9048096a227ae907
Lex Mel <lex07672@gmail.com>
2025-07-04 19:36:24 +0300

    fix: reduce the horizontal spread of baloons

--- src/utils/BackgroundAnimation/GamePage/Baloons/balloonUtils.ts
+++ src/utils/BackgroundAnimation/GamePage/Baloons/balloonUtils.ts
@@ -3,7 +3,7 @@ import { BALLOON_COLORS, Balloon } from './balloonTypes';
 export const createBalloon = (startFresh = false): Balloon => ({
   id: Date.now().toString(36) + Math.random().toString(36).substring(2),
   color: BALLOON_COLORS[Math.floor(Math.random() * BALLOON_COLORS.length)],
-  left: Math.random() * 95,
+  left: Math.random() * 85,
   duration: Math.random() * 20 + 7,
   delay: startFresh ? 0 : -1 * (Math.random() * 50 + 50),
   isPopping: false,
