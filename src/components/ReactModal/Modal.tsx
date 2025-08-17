f996952d3ab21040f744be96400bb50e43ff16bf
Lex Mel <lex07672@gmail.com>
2025-08-17 16:09:43 +0300

    feat: separeted styles from modal.tsx and small style change in it

--- src/components/ReactModal/Modal.tsx
+++ src/components/ReactModal/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import './Modal.css';
 
 type ModalProps = {
   isOpen: boolean;
@@ -20,33 +21,10 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div
-      className="modal-overlay"
-      onClick={onClose}
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        zIndex: 1000,
-      }}
-    >
+    <div className="modal-overlay" onClick={onClose}>
       <div
         className="modal-content"
         onClick={(e) => e.stopPropagation()} // предотвращает закрытие при клике внутри
-        style={{
-          background: '#fff',
-          padding: '20px',
-          borderRadius: '8px',
-          maxWidth: '90%',
-          maxHeight: '90%',
-          overflowY: 'auto',
-        }}
       >
         {children}
       </div>
