b3a566b088fd53c70cddd35d227cf5d69891d920
Lex Mel <lex07672@gmail.com>
2025-09-08 15:56:00 +0300

    feat(user-start-btn): add tooltip to StartPageMenu (no build update)

--- src/header-mainPage/UserStartPage/StartPageMenu.tsx
+++ src/header-mainPage/UserStartPage/StartPageMenu.tsx
@@ -1,9 +1,16 @@
+import Tooltip from '../../utils/Tooltip';
 // import './StartPageMenu.css';
 
 export default function StartPageMenu() {
   return (
     <div className="startpage-modal__content">
-      <h2>Меню стартовой страницы</h2>
+      <h2>
+        Меню стартовой страницы
+        <br />
+        <Tooltip text="Приложение открывается с стартовой страницы">
+          <span className="page-tooltipTriger">?</span>
+        </Tooltip>
+      </h2>
       {/* <p>Здесь будет функционал для настройки стартовой страницы.</p> */}
     </div>
   );
