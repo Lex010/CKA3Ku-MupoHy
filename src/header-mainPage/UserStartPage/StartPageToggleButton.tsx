e24c384ce004d39bc2f0c66a5d2e9237da7affc8
Lex Mel <lex07672@gmail.com>
2025-09-14 14:28:17 +0300

    feat(user-start-btn): the confirmation text when assigning a starting position has been clarified (no build update)

--- src/header-mainPage/UserStartPage/StartPageToggleButton.tsx
+++ src/header-mainPage/UserStartPage/StartPageToggleButton.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { setStartPage, clearStartPage } from '../../utils/forHeader/startPageUtils';
 import ConfirmPopup from '../../components/ConfirmPopup/ConfirmPopup';
 import { START_PAGE_UNSET_LABEL } from './startPageConstants';
+import { getReadablePageTitle } from '../../utils/forHeader/getReadablePageTitle';
 // import './css/StartPageToggleButton.css';
 
 interface StartPageToggleButtonProps {
@@ -41,10 +42,12 @@ export default function StartPageToggleButton({ startPage, onChange }: StartPage
         isOpen={showConfirm}
         onConfirm={isCurrentStart ? removeStartPage : assignStartPage}
         onCancel={() => setShowConfirm(false)}
-        title={isCurrentStart ? 'Удалить стартовую страницу?' : 'Назначить стартовую страницу?'}
+        title={
+          isCurrentStart ? 'Удалить стартовую страницу?' : `Назначить «${getReadablePageTitle(currentPage)}» стартовой?`
+        }
         message={
           isCurrentStart
-            ? `После удаления при запуске будет открываться ${START_PAGE_UNSET_LABEL}.`
+            ? `После удаления при запуске будет открываться «${START_PAGE_UNSET_LABEL}».`
             : `Сделать текущую страницу стартовой?`
         }
         confirmText="Да"
